54e91b3c610ef18a9e3012c9c44ddc3077d5aa59
Evan Sharp <ego@evasnahrp.ca>
2020-05-14 15:32:16 -0700

    footer stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,8 @@
 	// Scrolly.
 		$('.scrolly').scrolly();
 
+	$('#footer .icon').click(function(){
+		alert("Seriously? This website wasn't elaborate enough? You think I set up a social media presence for this gag? Sorry to disapoint.");
+	});
+
 })(jQuery);
